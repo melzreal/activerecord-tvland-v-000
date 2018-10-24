c71c2f70099b4a48d071401dbbff601329378159
Melissa Real <melissa.real@hotmail.com>
2018-10-24 15:27:19 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,6 +4,6 @@ class Character < ActiveRecord::Base
   belongs_to :show
 
   def say_that_thing_you_say
-    puts "#{self.name}  #{self.catchphrase}"
+     "#{self.name}  #{self.catchphrase}"
   end
 end
